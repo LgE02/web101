b2527824f99e9f199535265590212587b5158375
GaEun Lee <leegaeun023@gmail.com>
2025-01-16 19:25:44 +0900

    3주차 test

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //스프링 부트의 자동 설정, 스프링 Bean 생성 및 읽기 등의 기능이 자동으로 설정
 public class Application {
     public static void main(String[] args) {
-
+        // project initialization
         SpringApplication.run(Application.class, args);
     }
 }
